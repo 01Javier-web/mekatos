4c8e6b9560e273bdac21f840cff23d4ee64ba527
01Javier-web <andres13ytoo@gmail.com>
2026-09-14 23:21:09 -0500

    Relacionar productos con opciones de bebidas

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -26,4 +26,9 @@ class Product extends Model
     {
         return $this->hasMany(OrderItem::class);
     }
+
+    public function beverageOptions(): HasMany
+    {
+        return $this->hasMany(BeverageOption::class)->orderBy('sort_order');
+    }
 }
